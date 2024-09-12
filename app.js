449693e819b48f2405b23ddfb23e1b59bacac22b
hassanshakil576 <hassanshakil3008@gmail.com>
2024-09-12 09:02:36 -0700

    if else and array completed

--- app.js
+++ app.js
@@ -289,6 +289,434 @@
 
 
 
+// CONDITIONAL STATEMENTS
+
+// Q1 Write a program to check whether the given input number is divisible by 3 or else shows a message (number is not divisible by 3)
+
+// var num = prompt("enter a number");
+
+
+//     if(num%3==0){
+//         alert("number is divisible by 3");
+//     }
+//     else{
+//        alert("number is not divisible by 3");
+//     }
+
+// Q2 Write a program to check whether the given input is an odd or even number
+
+// var num = prompt("Enter a number");
+
+// if(num%2==0){
+//     alert("this number is even")
+// }
+// else{
+//     alert("this number is odd")
+// }
+
+// Q3 write an if/else statement with the following condition: If the variable age is greater than 18, output "old enough", otherwise output "Too young".
+
+// var age = prompt("enter your age");
+
+// if(age >= 18){
+//     console.log("old enough");
+    
+// }else{
+//     console.log("too young");
+    
+// }
+
+// Q4 
+
+// var name = "Hassan";
+// var username = prompt("Enter your name");
+
+// if(name==username){
+//     alert("Oh!What a coincidence.Your name is same as mine...")
+// }
+// else{
+    
+// }
+
+// Q5
+
+// var num = prompt("enter a number");
+
+
+//     if(num%3==0){
+//         alert("number is divisible by 3");
+//     }
+//     else{
+//        alert("number is not divisible by 3");
+//     }
+
+
+// Q6
+// var input =prompt("Enter")
+// var firstAplha = input.slice().toUpperCase()
+// var secondAplha = input.slice().toLowerCase()
+// if(input>=0){
+//     alert("It is a number")
+// }
+// else if(input==firstAplha){
+//     alert("It is a upper case")
+// }
+// else {
+//     alert("It is a lower case")
+// }
+
+// Q7
+// var first =+prompt("Enter first number")
+// var second =+prompt("Enter second number")
+// var operator =prompt("Enter operator")
+// if(operator=="+"){
+//     alert(first+second)
+// }
+// else if(operator=="-"){
+//     alert(first-second)
+// }
+// else if(operator=="*"){
+//     alert(first*second)
+// }
+// else if(operator=="/"){
+//     alert(first/second)
+// }
+// else if(operator=="%"){
+//     alert(first%second)
+// }
+// else {
+//     alert("Please enter input")
+// }
+
+// Q8
+// var time =prompt("Enter time according to 24 hrs format")
+// if(time> 6){
+//     alert("Good night")
+// }
+// else if(time<12){
+//     alert("Good Morning")
+// }
+// else if(time<16){
+//     alert("Good afternoon")
+// }
+// else if(time<19){
+//     alert("Good evening")
+// }
+// else if(time<24){
+//     alert("Good night")
+// }
+
+// Q9
+
+// var year = +prompt("Enter year");
+// if(year==2027 || year==2031){
+//     alert("It is a leap year")
+// }
+// else{
+//     alert("It is not a leap year")
+// }
+
+// Q10
+// var password = "karachi";
+// var enterPassword = prompt("enter your password");
+// if(password == enterPassword){
+//     alert("Correct! Password")
+// }
+// else if(password != enterPassword){
+//     alert("incorrect passowrd")
+// }
+// else{
+//     alert("please enter your password")
+// }
+
+// Q11
+// var firstName = "Ali";
+// if (firstName === "Fahad") {
+// document.write("Hello Fahad!");
+// }
+// else{
+//     document.write("You are not Fahad")
+// }
+
+// Q12
+// var hour = 13;
+// if (hour < 18) {
+// console.log(greeting = "Good day");
+// }
+// else {
+// console.log(greeting = "Good evening");
+// }
+
+// Q13
+// var num1 = +prompt("Enter a number");
+// var num2 = +prompt("Enter a number");
+// if(num1>num2){
+//     alert(+num1 + "is larger than " +num2)
+// }
+// else if(num1<num2){
+//     alert(+num2 + "is larger than " +num1)
+// }
+// else{
+//     alert("bothe the numbers are equal")
+// }
+
+// Q14
+// var num1 = +prompt("enter a number");
+// if(num1>0){
+//     alert("the number is positive")
+// }
+// else if(num1<0){
+//     alert("the number is negative");
+// }
+// else{
+//     alert("the number is zero")
+// }
+
+// Q15
+
+// var hour = +prompt("What the current hour is in 24 hours format");
+// if(hour<6){
+//     alert("sorry you will have to wait ya zyada bhook lagi hai tou apne liye bazaar se le aya")
+// }
+// else if(hour<9){
+//     alert("breakfast is served")
+// }
+// else if(hour<13){
+//     alert("Time for lunch")
+// }
+// else if(hour<17){
+//     alert("Its dinner time")
+// }
+// else{
+//     alert("please enter the hour within 17")
+// }
+
+// Q16
+// var enter =prompt("Enter")
+// if(enter>=0){
+//     alert("number")
+// }
+// else if(enter=="true" || enter=="false"){
+//     alert("boolean")
+// }
+// else {
+//     alert("string")
+// }
+
+// Q17
+// var enter = prompt("enter a letter");
+// if(enter== "a"){
+//     alert("True! its a wovel");
+// }
+// else if(enter== "e"){
+//     alert("True! its a wovel");
+// }
+// else if(enter== "i"){
+//     alert("True! its a wovel");
+// }
+// else if(enter== "o"){
+//     alert("True! its a wovel");
+// }
+// else if(enter== "u"){
+//     alert("True! its a wovel");
+// }
+// else{
+//     alert("false! not a wovel");
+// }
+
+// Q18
+// if(10!=8){
+//     alert("true")
+// }
+
+// Q19
+// var month =+prompt("Number of month")
+// if(month==1){
+//      alert("January")
+// }
+// else if(month==2){
+//     alert("Feburary")
+// }
+// else if(month==3){
+//     alert("March")
+// }
+// else if(month==4){
+//     alert("April")
+// }
+// else if(month==5){
+//     alert("May")
+// }
+// else if(month==6){
+//     alert("June")
+// }
+// else if(month==7){
+//     alert("July")
+// }
+// else if(month==8){
+//     alert("August")
+// }
+// else if(month==9){
+//     alert("September")
+// }
+// else if(month==10){
+//     alert("October")
+// }
+// else if(month==11){
+//     alert("November")
+// }
+// else if(month==12){
+//     alert("December")
+// }
+// else{
+//     alert("Invalid month no.")
+// }
+
+
+
+// Q20
+
+// var age = +prompt("enter your age");
+
+
+// if(age<18){
+//     alert("Too young")
+// }
+// else{
+//     alert("old enough")
+// }
+
+
+// "ARRAY"
+
+// Q1-Q7
+// var literalNotation = [];
+// var objectNotation = new Array();
+// var strArr = ["Hassan" , "Shakil"];
+// var numarr = [2];
+// var booleanarr = [true];
+// var mixedArr = ["Hassan" , 5 , true];
+// var mobileNetworks = ["Jazz" , "Telenor" , "ufone" , "warid" , "zong"];
+
+// Q8
+// var arr =["SSC","HSC","BCS","BS","BCOM","MS","M.Phil","phD"];
+// document.write(`<h1>QUALIFICATIONS:</h1> 1) ${arr[0]}<br> 2) ${arr[1]}<br>3) ${arr[2]}<br>4) ${arr[3]}<br>5) ${arr[4]}<br>6) ${arr[5]}<br>7) ${arr[6]}<br>8) ${arr[7]}<br>`)
+
+// Q9
+// var arr = [];
+// arr.push("Avengers: Age of Ultron");
+// arr.push("Spectre");
+// arr.push("Jurassic world");
+// arr.push("Inside out");
+// document.write(`<h1>Top movies of 2015</h1>1) ${arr[0]} <br>2) ${arr[1]} <br>3) ${arr[2]} <br>4) ${arr[3]}<h1>Length of array:${arr.length}</h1>`)
+
+// Q10
+// var cars =["Bmw","Mercedes","Audi","G-Wagon"]
+// document.write(`<h1>Favourite cars</h1>${cars}<br>First index of the array:0 <br> Car at first index of the array:${cars[0]}<br>Last index of the array:3 <br>Car at last index of the array: ${cars[3]} `)
+
+// Q11
+
+// var studentNames = ["micheal" , "John" , "Tony"];
+// var scores = [320 , 230 , 480];
+// var percent1 = 320*100/500;
+// var percent2 = 230*100/500;
+// var percent3 = 480*100/500;
+// document.write(`Score of ${studentNames[0]} is ${scores[0]} .Percentage:${percent1}% <br>Score of ${studentNames[1]} is ${scores[1]} .Percentage:${percent2}% <br>Score of ${studentNames[2]} is ${scores[2]} .Percentage:${percent3}% <br>`)
+
+// Q12
+// part "a"
+// var colours = ["black" , "blue" , "white" , "purple"];
+// var prom = prompt("enter a colour");
+// colours.unshift(prom)
+// console.log(colours);
+
+// part "b"
+// var colours = ["black" , "blue" , "white" , "purple"];
+// var prom = prompt("enter a colour");
+// colours.push(prom)
+// console.log(colours);
+
+// part "c"
+// var colours = ["black" , "blue" , "white" , "purple"];
+// var color1 =prompt("Enter color");
+// var color2 =prompt("Enter one more color");
+// colours.unshift(color1)
+// colours.unshift(color2);
+// console.log(colours);
+
+// part "d"
+// var colours = ["black" , "blue" , "white" , "purple"];
+// colours.shift()
+// console.log(colours);
+
+// part "e"
+// var colours = ["black" , "blue" , "white" , "purple"];
+// arr.splice(add,0,add2);
+// colours.pop()
+// console.log(colours);
+
+// part "f"
+// var colours = ["black" , "blue" , "white" , "purple"];
+// var add =prompt("Enter index no. at which you want to add color")
+// var add2 =prompt("Enter color name what you want to add")
+// colours.splice(add,0,add2);
+// console.log(colours.splice(add,add2));
+
+// part "g"
+// var colours = ["black" , "blue" , "white" , "purple"];
+// var delete =prompt("Enter index no. at which you want to add color")
+// var delete2 =prompt("Enter color name what you want to add")
+// colours.splice(delete,delete2);
+// console.log(colours.splice(delete,delete2));
+
+// Q13
+
+// var studentScores = [320 , 230 , 480 ,120];
+// console.log(studentScores.sort());
+
+// Q14
+// var fruits = ["strawberry", "apple", "orange", "banana"]
+// console.log(fruits.sort());
+
+// Q15
+// var cityNames = ["Karachi" , "lahore" , "Islamabad" , "quetta" , "Peshawar"];
+// var selectedCities = cityNames.slice(2 , 4);
+// console.log(selectedCities);
+
+// Q16
+// var arr = ["this" , "is" , "my" , "cat"];
+// console.log(arr.join(" "));
+
+// Q17
+// var arr = [];
+// arr.push("Keyboard");
+// arr.push("Mouse");
+// arr.push("Pointer");
+// arr.push("Monitor");
+// document.write(`Devices:<br>${arr}<br>Out:<br>${arr[0]}<br>Out:<br>${arr[1]}<br>Out:<br>${arr[2]}<br>Out:<br>${arr[3]}`)
+
+// Q18
+// var arr = [];
+// arr.push("Keyboard");
+// arr.push("Mouse");
+// arr.push("Pointer");
+// arr.push("Monitor");
+// document.write(`Devices:<br>${arr}<br>Out:<br>${arr[3]}<br>Out:<br>${arr[2]}<br>Out:<br>${arr[1]}<br>Out:<br>${arr[0]}`)
+
+// Q19
+// var arr =["Apple","Samsung","Motorolla","Nokia","Sony","Haier"]
+// document.write(`<select><option>${arr[0]}</option><option>${arr[1]}</option><option>${arr[2]}</option><option>${arr[3]}</option><option>${arr[4]}</option><option>${arr[5]}</option></select>`)
+
+// Q20-Q21
+// var arr =[["0","1","2","3"],["1","0","1","2"],["2","1","0","1"]]
+// document.write(arr[0]+"<br>"+arr[1]+"<br>"+arr[2])
+
+
+
+
+
+
+
+
 
 
 
